35e2217f06c32b07a678f2adf9d090ac718c2354
Miki Tzenkov <cpalma@vektortechnologies.com>
2020-10-26 00:09:32 -0300

    Serilog Logging System & Separate Command Handler Class

---
+++ DC-Mars/Core/CommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DC_Mars.Core
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+
+        public CommandHandler(DiscordSocketClient client, CommandService commands)
+        {
+            _commands = commands;
+            _client = client;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            //Don't process as sysmsg
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            int argPos = 0;
+
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                  message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                  message.Author.IsBot)
+                return;
+
+            var context = new SocketCommandContext(_client, message);
+
+            await _commands.ExecuteAsync(context: context, argPos: argPos, services: null);
+        }
+    }
+}
--- DC-Mars/Program.cs
+++ DC-Mars/Program.cs
@@ -2,6 +2,9 @@
 using Discord;
 using Discord.WebSocket;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Core;
+using DC_Mars.Debug;
 
 namespace DC_Mars
 {
@@ -11,15 +14,15 @@ namespace DC_Mars
         private static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
         private DiscordSocketClient _client;
+        private Logging logger = new Logging();
 
         public async Task MainAsync()
         {
+            Console.Title = "DC-MARS";
             // Create new client for connecting to the Discord Gateway
             _client = new DiscordSocketClient();
-            _client.MessageReceived += CommandHandler;
-
-            // Add an Event to our Log, so we can log events and actions that happen.
-            _client.Log += Log;
+            // Send log events to our Logging class
+            _client.Log += logger.LogSys;
 
             // Discord Bot Token
             var token = "NzY5ODQ3MjUyMzczNjAyMzMy.X5U-IA.q06A94q3MTlEJ71K8TGWitwML8s";
@@ -28,36 +31,6 @@ namespace DC_Mars
             await _client.StartAsync();
 
             await Task.Delay(-1);
-        }
-
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
-
-        private Task CommandHandler(SocketMessage message)
-        {
-            string command = "";
-            int lengthOfCommand = -1;
-
-            if (!message.Content.StartsWith('!')) //Bot command prefix
-                return Task.CompletedTask;
-
-            if (message.Author.IsBot) //Ignore all commands from other bots
-                return Task.CompletedTask;
-
-            if (message.Content.Contains(' '))
-                lengthOfCommand = message.Content.IndexOf(' ');
-            else
-                lengthOfCommand = message.Content.Length;
-
-            command = message.Content.Substring(1, lengthOfCommand - 1).ToLower();
-
-            if (command.Equals("hello"))
-                message.Channel.SendMessageAsync($@"Hello {message.Author.Mention}");
-
-            return Task.CompletedTask;
         }
     }
 }
