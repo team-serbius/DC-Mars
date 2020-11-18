408a118cc9b9359be428e050a02e0f26d0feee36
Miki Tzenkov <cpalma@vektortechnologies.com>
2020-11-18 18:38:40 -0300

    ErrorHandler
    Note: Multiple changes still not committed.

---
+++ DC-Mars/Debug/ErrorHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DC_Mars.Debug
+{
+    public class ErrorHandler
+    {
+        private Logging logger = new Logging();
+
+        private int ErrorCount;
+        private string ErrorReason;
+
+        public int GetErrorCounter()
+        {
+            return ErrorCount;
+        }
+
+        public string GetErrorReason()
+        {
+            return ErrorReason;
+        }
+
+        public void IncrementError(int val)
+        {
+            ErrorCount = ErrorCount + val;
+            logger.LogCustom("Error count since Execution: " + val, 2);
+        }
+
+        public void SetErrorReason(string error)
+        {
+            ErrorReason = error;
+        }
+    }
+}
